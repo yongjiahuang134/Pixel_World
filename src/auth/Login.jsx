7244b2f38f9a30ae0770c6e9b5885dc34836c969
Yongjia Huang <yongjiahuang134@gmail.com>
2023-11-14 20:57:32 -0800

    Username and Password must be numbers/letters

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -7,7 +7,36 @@ export const Login = (props) => {
     const [username, setusername] = useState('');
     const [pass, setpass] = useState('');
     const history = useNavigate();
+    function checkValidUsername() {
+        let textbox = document.getElementById('username');
+        let errorSpan = document.getElementById('error');
+    
+        if (textbox.value.length === 0) {
+          errorSpan.textContent = 'Need to input something for your username';
+          textbox.style.border = '1px solid #FF0000'; // Apply red border
+        } else if (!/^[a-zA-Z0-9]+$/.test(textbox.value)) {
+            errorSpan.textContent = 'Username can only contain letters and numbers';
+        } else {
+          errorSpan.textContent = ''; // Clear error message
+          textbox.style.border = '1px solid #ced4da'; // Reset border to default
+        }
 
+    }
+    function checkValidPass(){
+        let textbox = document.getElementById('password');
+        let errorSpan = document.getElementById('errorpass');
+    
+        if (textbox.value.length === 0) {
+          errorSpan.textContent = 'Need to input something for your Password';
+          textbox.style.border = '1px solid #FF0000'; // Apply red border
+        } else if (!/^[a-zA-Z0-9]+$/.test(textbox.value)) {
+            errorSpan.textContent = 'Password can only contain letters and numbers';
+        } else {
+          errorSpan.textContent = ''; // Clear error message
+          textbox.style.border = '1px solid #ced4da'; // Reset border to default
+        }
+
+    }
     //set up backend server
     async function handleSubmit(e) {
         e.preventDefault();
@@ -43,9 +72,11 @@ export const Login = (props) => {
             <h1>Log In</h1>
             <form action="POST" className="login-form" onSubmit={handleSubmit}>
                 <label htmlFor="username"> Username: </label>
-                <input value={username} onChange={(e) => setusername(e.target.value)} type="text" placeholder="joebruin" id="username" name="username" />
+                <input value={username} onChange={(e) => setusername(e.target.value)} type="text" placeholder="joebruin" id="username" name="username" onKeyUp={checkValidUsername}/>
+                <span style={{ color: '#FF0000' }} id='error'></span>
                 <label htmlFor="password"> Password: </label>
-                <input value={pass} onChange={(e) => setpass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+                <input value={pass} onChange={(e) => setpass(e.target.value)} type="password" placeholder="********" id="password" name="password" onKeyUp={checkValidPass}/>
+                <span style={{ color: '#FF0000' }} id='errorpass'></span>
                 <button class="button" type="submit"><span>Log In</span></button>
             </form>
             <Link to='/signup'>Don't have an account? Click here to sign up!</Link>
--- src/auth/Signup.jsx
+++ src/auth/Signup.jsx
@@ -7,6 +7,36 @@ export const Signup = (props) => {
     const [pass, setpass] = useState('');
     const history = useNavigate();
     //set up backend server
+    function checkValidUsername() {
+        let textbox = document.getElementById('username');
+        let errorSpan = document.getElementById('error');
+    
+        if (textbox.value.length === 0) {
+          errorSpan.textContent = 'Need to input something for your username';
+          textbox.style.border = '1px solid #FF0000'; // Apply red border
+        } else if (!/^[a-zA-Z0-9]+$/.test(textbox.value)) {
+            errorSpan.textContent = 'Username can only contain letters and numbers';
+        } else {
+          errorSpan.textContent = ''; // Clear error message
+          textbox.style.border = '1px solid #ced4da'; // Reset border to default
+        }
+
+    }
+    function checkValidPass(){
+        let textbox = document.getElementById('password');
+        let errorSpan = document.getElementById('errorpass');
+    
+        if (textbox.value.length === 0) {
+          errorSpan.textContent = 'Need to input something for your Password';
+          textbox.style.border = '1px solid #FF0000'; // Apply red border
+        } else if (!/^[a-zA-Z0-9]+$/.test(textbox.value)) {
+            errorSpan.textContent = 'Password can only contain letters and numbers';
+        } else {
+          errorSpan.textContent = ''; // Clear error message
+          textbox.style.border = '1px solid #ced4da'; // Reset border to default
+        }
+
+    }
     async function handleSubmit(e) {
         e.preventDefault();
         try{
@@ -35,9 +65,11 @@ export const Signup = (props) => {
             <h1>Sign Up</h1>
             <form className="signup-form" onSubmit={handleSubmit}>
                 <label htmlFor="username"> Username: </label>
-                <input value={username} onChange={(e) => setusername(e.target.value)} type="text" placeholder="joebruin" id="username" name="username" />
+                <input value={username} onChange={(e) => setusername(e.target.value)} type="text" placeholder="joebruin" id="username" name="username" onKeyUp={checkValidUsername} />
+                <span style={{ color: '#FF0000' }} id='error'></span>
                 <label htmlFor="password"> Password: </label>
-                <input value={pass} onChange={(e) => setpass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+                <input value={pass} onChange={(e) => setpass(e.target.value)} type="password" placeholder="********" id="password" name="password" onKeyUp={checkValidPass} />
+                <span style={{ color: '#FF0000' }} id='errorpass'></span>
                 <button class="button" type="submit"><span>Sign Up</span></button>
             </form>
             <Link to='/login'>Already have an account? Log In here!</Link>
