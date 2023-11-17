9447dcd679f67e32249a06885b17542a8cb57e26
Zhang Jing Ran <zhangjingran2003@gmail.com>
2023-11-17 15:15:05 -0800

    add image upload to home page

--- src/Frontend/Home.js
+++ src/Frontend/Home.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 //Main Page after login/signup
-function Home(){
-    
+
+function Home() {
+    const [image, setImage] = useState(null);
+
+    const handleImageUpload = (event) => {
+        const file = event.target.files[0];
+        const reader = new FileReader();
+
+        reader.onloadend = () => {
+            setImage(reader.result);
+        }
+
+        reader.readAsDataURL(file);
+    }
+
     return (
         <div className='MainPage'>
             <h1>Welcome to Home page</h1>
-            
+            <input type='file' accept='image/*' id='fileUpload' onChange={handleImageUpload} style={{display: 'none'}} />
+            <label htmlFor='fileUpload' style={{cursor: 'pointer'}}>Upload Image</label>
+            {image && <img src={image} alt='Uploaded for editing' />}
         </div>
     );
 }
