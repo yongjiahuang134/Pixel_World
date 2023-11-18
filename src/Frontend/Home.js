903b9ce9054b9720712eee8890a2dfe8791a9ee7
Zhang Jing Ran <zhangjingran2003@gmail.com>
2023-11-17 16:26:58 -0800

    Add Download as .png function/button to Home.js

--- src/Frontend/Home.js
+++ src/Frontend/Home.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 //Main Page after login/signup
 
+
 function Home() {
     const [image, setImage] = useState(null);
 
@@ -16,14 +17,27 @@ function Home() {
         reader.readAsDataURL(file);
     }
 
+    const handleDownload = () => {
+        const link = document.createElement('a');
+        link.href = image;
+        link.download = 'download.png';
+        link.click();
+    }
+
+    // TODO: Change pallete to resizeble box
     return (
         <div className='MainPage'>
             <h1>Welcome to Home page</h1>
             <input type='file' accept='image/*' id='fileUpload' onChange={handleImageUpload} style={{display: 'none'}} />
             <label htmlFor='fileUpload' style={{cursor: 'pointer'}}>Upload Image</label>
-            {image && <img src={image} alt='Uploaded for editing' />}
+            {image && <div style={{width: '500px', height: '500px'}}>
+                <img src={image} alt='Uploaded for editing' style={{maxWidth: '100%', maxHeight: '100%'}} />
+            </div>}
+            {image && <button onClick={handleDownload}>Download Image</button>}
+            
         </div>
     );
 }
 
 export default Home;
+
