28ec6ac54c8b0831b22d421ece4dfac2750260f9
Mochimomomo <149128441+Mochimomomo@users.noreply.github.com>
2023-11-30 14:54:37 -0800

    Update script.js to fit to React environment

--- src/script.js
+++ src/script.js
@@ -1,23 +1,52 @@
+import React, { useState } from 'react';
+import './styles.css';
+
 // Define the size of the grid
-var gridSize = 10;
+const gridSize = 10;
 
 // Initialize a blank grid
-var grid = Array(gridSize).fill().map(() => Array(gridSize).fill("#ffffff"));
+const blankGrid = Array(gridSize).fill().map(() => Array(gridSize).fill("#ffffff"));
 
-var table = document.getElementById('grid');
-for (var i = 0; i < gridSize; i++) {
-  var row = document.createElement('tr');
-  for (var j = 0; j < gridSize; j++) {
-    var cell = document.createElement('td');
-    cell.style.backgroundColor = grid[i][j];
-    cell.addEventListener('click', (function(i, j) {
-      return function() {
-        var color = document.getElementById('colorPicker').value;
-        grid[i][j] = color;
-        cell.style.backgroundColor = color;
-      };
-    })(i, j));
-    row.appendChild(cell);
-  }
-  table.appendChild(row);
+function App() {
+  const [grid, setGrid] = useState(blankGrid);
+  const [color, setColor] = useState("#000000");
+
+  const changeColor = (row, col) => {
+    const newGrid = JSON.parse(JSON.stringify(grid));
+    newGrid[row][col] = color;
+    setGrid(newGrid);
+  };
+
+  return (
+    <div>
+      <ColorPicker color={color} setColor={setColor} />
+      <Grid grid={grid} changeColor={changeColor} />
+    </div>
+  );
 }
+
+function ColorPicker({ color, setColor }) {
+  return (
+    <input type="color" value={color} onChange={(e) => setColor(e.target.value)} />
+  );
+}
+
+function Grid({ grid, changeColor }) {
+  return (
+    <table>
+      {grid.map((row, i) =>
+        <tr key={i}>
+          {row.map((col, j) => (
+            <td
+              key={`${i}-${j}`}
+              onClick={() => changeColor(i, j)}
+              style={{ background: grid[i][j] }}
+            />
+          ))}
+        </tr>
+      )}
+    </table>
+  );
+}
+
+export default App;
