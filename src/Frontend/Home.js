f1d83318f8fd3a920b831ff16962c56f2e8b51af
Jordan Jiang <jyylasdszer25@g.ucla.edu>
2023-12-01 03:47:47 -0800

    add pixel function circular

--- src/Frontend/Home.js
+++ src/Frontend/Home.js
@@ -317,6 +317,106 @@ function Home() {
             }
         }
     };
+
+    // 1
+const handleCircleBasedPixelization = () => {
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    const img = new Image();
+    img.src = image;
+    img.onload = () => {
+      canvas.width = img.naturalWidth;
+      canvas.height = img.naturalHeight;
+      ctx.drawImage(img, 0, 0);
+      circleBasedPixelization(ctx, img, 300, 30, 70); // Example values for innerRadius, innerScale, and outerScale
+      const pixelatedDataUrl = canvas.toDataURL();
+      setImage(pixelatedDataUrl);
+    };
+    img.onerror = () => {
+      console.error("Error loading image for pixelation");
+    };
+  };
+
+  function isInsideCircle(x, y, centerX, centerY, radius) {
+    return (x - centerX) ** 2 + (y - centerY) ** 2 <= radius ** 2;
+}
+
+function circleBasedPixelization(ctx, img, innerRadius, innerScale, outerScale) {
+    const width = img.width;
+    const height = img.height;
+    const centerX = width / 2;
+    const centerY = height / 2;
+    const radiusSquared = innerRadius * innerRadius;
+
+    ctx.drawImage(img, 0, 0, width, height);
+    const imageData = ctx.getImageData(0, 0, width, height);
+    const data = imageData.data;
+
+    // Function to check if a point is within the inner circle
+    function isInsideCircle(x, y) {
+        const dx = x - centerX;
+        const dy = y - centerY;
+        return dx * dx + dy * dy <= radiusSquared;
+    }
+
+    // Function to apply pixelization
+    function applyPixelization(startX, startY, endX, endY, scale) {
+        for (let x = startX; x < endX; x += scale) {
+            for (let y = startY; y < endY; y += scale) {
+                let sums = [0, 0, 0];
+                let count = 0;
+
+                // Collect pixel data within the block
+                for (let i = 0; i < scale; i++) {
+                    for (let j = 0; j < scale; j++) {
+                        if (x + i < width && y + j < height) {
+                            const index = ((y + j) * width + (x + i)) * 4;
+                            sums[0] += data[index];     // R
+                            sums[1] += data[index + 1]; // G
+                            sums[2] += data[index + 2]; // B
+                            count++;
+                        }
+                    }
+                }
+
+                // Calculate and apply the average color to the block
+                if (count > 0) {
+                    const avg = sums.map(s => s / count);
+                    for (let i = 0; i < scale; i++) {
+                        for (let j = 0; j < scale; j++) {
+                            if (x + i < width && y + j < height) {
+                                const index = ((y + j) * width + (x + i)) * 4;
+                                data[index] = avg[0];     // R
+                                data[index + 1] = avg[1]; // G
+                                data[index + 2] = avg[2]; // B
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // Apply pixelization to the inner circle first
+    applyPixelization(0, 0, width, height, innerScale);
+
+    // Apply pixelization to the area outside the inner circle
+    for (let x = 0; x < width; x += outerScale) {
+        for (let y = 0; y < height; y += outerScale) {
+            // Check if the current block overlaps with the inner circle
+            const overlapsInner = isInsideCircle(x, y) || isInsideCircle(x + outerScale, y) ||
+                                  isInsideCircle(x, y + outerScale) || isInsideCircle(x + outerScale, y + outerScale);
+            if (!overlapsInner) {
+                applyPixelization(x, y, x + outerScale, y + outerScale, outerScale);
+            }
+        }
+    }
+
+    ctx.putImageData(imageData, 0, 0);
+}
+
+
+// 2
     
     const calculateAverageColorBW = (ctx, startX, startY, blockSize, width, height) => {
         let total = { r: 0, g: 0, b: 0, a: 0 };
@@ -382,6 +482,7 @@ function Home() {
 
                     {(image || pixelImage) && <button className="feature-button" onClick={handlePixelate}>Pixelate Image</button>}
                     {(image || pixelImage) && <button className="feature-button" onClick={handlePixelateBW}>Black & White</button>}
+                    {(image || pixelImage) && <button className="feature-button" onClick={handleCircleBasedPixelization}>Circular</button>}
                     {(image || pixelImage) && <button className="feature-button" onClick={handleProcessImage}>Process Image</button>}
                 </div>
             </div>
