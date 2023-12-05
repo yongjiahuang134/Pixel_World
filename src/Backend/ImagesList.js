69067eb75a9abea408ba48784ba98adb01798930
Zhang Jing Ran <zhangjingran2003@gmail.com>
2023-12-04 17:08:34 -0800

    create Gallery.css for Gallery page & add searchbar for searching images in the Gallery

--- src/Backend/ImagesList.js
+++ src/Backend/ImagesList.js
@@ -1,17 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, Link, useLocation } from 'react-router-dom';
 import logo from '../Frontend/logo.jpg'
+import './Gallery.css'
 
 function ImagesList() {
     const [images, setImages] = useState([]);
+    const [searchString, setSearchString] = useState('');
     const { username } = useParams();
     const location = useLocation();
+
     useEffect(() => {
         fetch(`http://localhost:8002/getimages/${username}`)
             .then(response => response.json())
             .then(data => setImages(data))
             .catch(error => console.error('Error:', error));
     }, [username, location]); 
+
+    const filteredImages = images.filter(image => image.imageName.toLowerCase().includes(searchString));
 
     return (
         <div>
@@ -21,8 +26,22 @@ function ImagesList() {
                 <h3>Tips:</h3>
                 <p className="Introduction">You can click the image name to continue your work on this specific image!</p> 
             </div>
+            <input
+                list="image-names"
+                type="text"
+                id="searchBar"
+                placeholder="Search images..."
+                onChange={(e) => setSearchString(e.target.value.toLowerCase())}
+            />
+            <datalist id="image-names">
+                {filteredImages.map((image, index) => (
+                    <option key={index} value={image.imageName}>
+                        <Link to={`/home/${username}?imageName=${image.imageName}`}>{image.imageName}</Link>
+                    </option>
+                ))}
+            </datalist>
             <div className="Gallery">
-                {images.map((image, index) => (
+                {filteredImages.map((image, index) => (
                     <div key={index} className="image-item">
                     <img src={`${image.imageData}`} alt={`Uploaded by ${image.username}`} style={{ maxWidth: '100%', maxHeight: '100%' }} />
                     <p>
